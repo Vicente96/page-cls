37cc799ef76c558441802e7c4cc325694ef1e7e4
Vicente96 <vicentematavelasco@gmail.com>
2025-07-25 17:35:59 -0600

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://vicente96.github.io/page-cls",
-})
+  base: "https://github.com/Vicente96/page-cls.git",
+});
